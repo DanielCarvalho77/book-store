479afd14e199413eacafcd238b3c0da7d6e0e186
Daniel Carvalho <danielcarrosa@gmail.com>
2024-07-22 21:57:26 -0300

    feat: back do book store

--- src/servicos/livros.js
+++ src/servicos/livros.js
@@ -1,11 +1,25 @@
-import axios from "axios";
+// import axios from "axios";
 
-const livrosAPI = axios.create({baseURL: 'http://localhost:8000/livros'});
+// const livrosAPI = axios.create({baseURL: 'http://localhost:8000/livros'});
 
-function getLivros() {
-    const response = livrosAPI.get('/');
+// function getLivros() {
+//     const response = livrosAPI.get('/');
 
-    return response.data;
+//     return response.data;
+// }
+
+// export {
+//     getLivros
+// }
+
+import axios from "axios"
+
+const livrosAPI = axios.create({baseURL: "http://localhost:8000/livros"})
+
+async function getLivros() {
+    const response = await livrosAPI.get('/')
+
+    return response.data
 }
 
 export {
